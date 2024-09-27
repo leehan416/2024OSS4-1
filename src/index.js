825e334d33965ba3791a1b538d9af1b4f26b618d
Whal3 <leehan416@gmail.com>
2024-09-28 00:15:05 +0900

    [update] update app.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import Test from './test';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <Test /> */}
   </React.StrictMode>
 );
 
